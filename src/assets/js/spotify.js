517ae733196c49c730d9ab057957e9821377df6d
svend <sven.depickere@student.howest.be>
2019-10-30 14:46:58 +0100

    spotify configuration

---
+++ src/assets/js/spotify.js
@@ -0,0 +1,41 @@
+/**
+ Created by svend on 30/10/2019.
+ **/
+
+export default (function () {
+
+    const configuration = {
+        authEndpoint: "https://accounts.spotify.com/authorize",
+        clientId: "a99753b000db451b9a40b2420e8cdfcb",
+        redirectUri: "http://localhost:8081/login",
+        scopes: [
+            "user-top-read",
+            // "user-read-email",
+            // "user-read-private"
+        ]
+    };
+
+    //******  HELPER FUNCTION ******//
+
+    function getHash() {
+        const hash = window.location.hash
+            .substring(1)
+            .split('&')
+            .reduce(function (initial, item) {
+                if (item) {
+                    var parts = item.split('=');
+                    initial[parts[0]] = decodeURIComponent(parts[1]);
+                }
+                return initial;
+            }, {});
+        window.location.hash = '';
+        return hash;
+    }
+
+
+    return {
+        configuration,
+        getHash
+    }
+
+})();

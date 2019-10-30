acc83169d1efeeef59875278a3224cc3a70b2254
svend <sven.depickere@student.howest.be>
2019-10-30 13:41:25 +0100

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,18 @@
 <template>
-
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+            <div class="my-2">
+                <v-btn large color="success">Login with
+                    <font-awesome-icon :icon="['fab', 'spotify']" size="lg" pull="right"></font-awesome-icon>
+                </v-btn>
+            </div>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
     export default {
-        name: "test"
+        data: () => ({})
     }
 </script>
 

c0c2b5054bd48e82667fb89d1f6220d527015bbf
svend <sven.depickere@student.howest.be>
2019-10-30 10:54:51 +0100

    format

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,12 @@
 </template>
 
 <script>
-    const defaultLayout = 'default'
+    const default_layout = 'default';
 
     export default {
         computed: {
             layout() {
-                return (this.$route.meta.layout || defaultLayout) + '-layout'
+                return (this.$route.meta.layout || default_layout) + '-layout'
             }
         }
     }
--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 // import createPersistedState from 'vuex-persistedstate'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
@@ -12,7 +12,7 @@ export default new Vuex.Store({
   //   paths: ["voted"]
   // })],
   mutations: {
-    updateLogin (state, newLogin) {
+    updateLogin(state, newLogin) {
       state.login = newLogin
     }
   },

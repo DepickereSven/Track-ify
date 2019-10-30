2051e18e98892e78a590f1e0eddcfa332692980c
svend <sven.depickere@student.howest.be>
2019-10-30 15:38:22 +0100

    Create Confirm.vue

---
+++ src/views/Confirm.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+
+    import handle from "../assets/js/Vue/confirm/handle"
+
+    export default {
+        created: function () {
+            handle.init(this);
+        },
+        data: () => ({}),
+        methods: {}
+    }
+</script>
+
+<style scoped>
+
+</style>

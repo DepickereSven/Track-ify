90e1a4d660c40d17dc1a4a65d199c056d8850b03
svend <sven.depickere@student.howest.be>
2019-10-30 10:55:17 +0100

    use UI

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <component :is="layout">
       <router-view></router-view>
     </component>
--- src/main.js
+++ src/main.js
@@ -6,10 +6,10 @@ import store from './store'
 import Default from './layouts/Default'
 import Empty from './layouts/Empty'
 
-// import ElementUI from 'element-ui'
-// import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
-// Vue.use(ElementUI);
+Vue.use(ElementUI);
 
 Vue.component('default-layout', Default);
 Vue.component('empty-layout', Empty);

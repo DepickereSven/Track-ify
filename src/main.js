cecba09a8c1c7be315d6939b561a174e2a9d717d
svend <sven.depickere@student.howest.be>
2019-10-30 12:25:52 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import vuetify from './plugins/vuetify';
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -14,5 +15,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app');

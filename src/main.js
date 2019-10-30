1d5498e5a93f2ba9821c6123f0346a0195ea0f0d
svend <sven.depickere@student.howest.be>
2019-10-30 11:13:03 +0100

    remove element UI

--- src/main.js
+++ src/main.js
@@ -6,11 +6,6 @@ import store from './store'
 import Default from './layouts/Default'
 import Empty from './layouts/Empty'
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.use(ElementUI);
-
 Vue.component('default-layout', Default);
 Vue.component('empty-layout', Empty);
 

54e769241720e1e8c5f8976cfce5ad571ce04f3a
svend <sven.depickere@student.howest.be>
2019-10-31 13:12:58 +0100

    find moste listen genres

--- src/assets/js/Vue/home/handle.js
+++ src/assets/js/Vue/home/handle.js
@@ -3,24 +3,27 @@
  **/
 
 import spotify from "../../spotify"
-import testFile from "../../../images/test"
+import songData from "../../../images/test"
+import artists from "../../../images/artists"
 
 export default (function () {
 
     const init = function (_self) {
-        // _self.data = testFile.items;
-        spotify.configuration.spotifyApi.getMyTopTracks({
-            limit: 50,
-            time_range: "short_term"
-        }).then(function(data) {
-            _self.data = data.items;
-        }).then(function() {
-            return spotify.configuration.spotifyApi.getArtists(getIDsArtists(_self));
-        }).then(function(artistsInfo) {
-            _self.artists = artistsInfo;
-        }).catch(function(error) {
-            console.error(error);
-        });
+        _self.data = songData.items;
+        _self.artists = artists.artists;
+        getAllGenres(_self)
+        // spotify.configuration.spotifyApi.getMyTopTracks({
+        //     limit: 50,
+        //     time_range: "short_term"
+        // }).then(function(data) {
+        //     _self.data = data.items;
+        // }).then(function() {
+        //     return spotify.configuration.spotifyApi.getArtists(getIDsArtists(_self));
+        // }).then(function(artistsInfo) {
+        //     _self.artists = artistsInfo;
+        // }).catch(function(error) {
+        //     console.error(error);
+        // });
 
     };
 
@@ -36,6 +39,36 @@ export default (function () {
         return array.map(function (el) {
             return el[0];
         });
+    }
+
+    function getAllGenres(_self) {
+        _self.genres = createNumberOfGenres(_self.artists.map(function (el) {
+            return el.genres;
+        }));
+    }
+
+    function createNumberOfGenres(genres) {
+        let genreArray = [];
+        genres.forEach(function (el) {
+            if (el.length !== 0){
+                el.forEach(function (element) {
+                    let index = genreArray.findIndex((genre => genre.name === element));
+                    if (index === undefined || index === -1){
+                        genreArray.push({
+                            name: element,
+                            hits: 1
+                        })
+                    } else {
+                        genreArray[index].hits++;
+                    }
+                })
+            }
+        });
+        return sort(genreArray);
+    }
+
+    function sort(list) {
+        return list.sort((a, b) => (a.hits < b.hits) ? 1 : -1)
     }
 
 

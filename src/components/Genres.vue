bc15c58bd192dbf30959f3dd001a37d079e96a24
svend <sven.depickere@student.howest.be>
2019-10-31 14:29:06 +0100

    Create Genres.vue

---
+++ src/components/Genres.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <doughnut-chart v-if="loaded" :chartdata="chartData" :options="options"></doughnut-chart>
+    </div>
+</template>
+
+<script>
+
+    import doughnutChart from "./charts/Doughnut"
+
+    export default {
+        name: "Genres",
+        components: {doughnutChart},
+        props: ["loaded", "chartData"],
+        data: () => ({
+            options: {responsive: true, maintainAspectRatio: false}
+        })
+    }
+</script>
+
+<style scoped>
+
+</style>

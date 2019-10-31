4982fdcc3a0cbc0b2a46b0ae622e9383412f207b
svend <sven.depickere@student.howest.be>
2019-10-31 12:30:04 +0100

    Create ArtistsList.vue

---
+++ src/components/ArtistsList.vue
@@ -0,0 +1,32 @@
+<template>
+        <div id="artistsName">
+            <div v-if="artists.length === 1">
+                <span>{{artists[0].name}}</span>
+            </div>
+            <div v-else v-for="(artist, i) in artists">
+                <span class="whitespace" v-if="artists.length > i + 1">{{artist.name}},</span>
+                <span v-else>{{artist.name}}</span>
+            </div>
+        </div>
+</template>
+
+<script>
+
+    export default {
+        name: "ArtistsList",
+        props: ["artists"],
+        created() {
+            console.log(this.$props.artists.length)
+        }
+    }
+
+
+</script>
+
+<style scoped>
+
+    #inspire .whitespace{
+        margin-right: 4px;
+    }
+
+</style>

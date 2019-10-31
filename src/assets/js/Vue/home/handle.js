c25aa1436e13915596d1be6ad6096858495177e9
svend <sven.depickere@student.howest.be>
2019-10-31 12:44:46 +0100

    get artists

--- src/assets/js/Vue/home/handle.js
+++ src/assets/js/Vue/home/handle.js
@@ -3,20 +3,40 @@
  **/
 
 import spotify from "../../spotify"
+import testFile from "../../../images/test"
 
 export default (function () {
 
     const init = function (_self) {
+        // _self.data = testFile.items;
         spotify.configuration.spotifyApi.getMyTopTracks({
             limit: 50,
             time_range: "short_term"
         }).then(function(data) {
             _self.data = data.items;
-            console.log(_self.data);
-        }, function(err) {
-            console.error(err);
+        }).then(function() {
+            return spotify.configuration.spotifyApi.getArtists(getIDsArtists(_self));
+        }).then(function(artistsInfo) {
+            _self.artists = artistsInfo;
+        }).catch(function(error) {
+            console.error(error);
         });
+
     };
+
+    const getIDsArtists = function (_self) {
+        return makeString(_self.data.map(function (el) {
+            return el.artists.map(function (elementje) {
+                return elementje.id;
+            });
+        }))
+    };
+
+    function makeString (array) {
+        return array.map(function (el) {
+            return el[0];
+        });
+    }
 
 
     return {

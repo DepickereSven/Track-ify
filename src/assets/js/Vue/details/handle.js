b526e16b4b42933841f20f1c5b67ba1f027da414
svend <sven.depickere@student.howest.be>
2019-11-04 13:06:21 +0100

    Update handle.js

--- src/assets/js/Vue/details/handle.js
+++ src/assets/js/Vue/details/handle.js
@@ -2,15 +2,27 @@
  Created by svend on 4/11/2019.
  **/
 
+import spotify from "../../spotify"
 import songData from "../../../images/detailsSongDetails"
 import artistsData from "../../../images/detailArtists"
+import album from "../../../images/detailsAlbum"
 
 export default (function () {
 
     const init = function (_self) {
         _self.songDetails = songData;
         _self.artists = artistsData;
+        _self.album = album;
+        // getAlbums(_self);
     };
+
+
+    function getAlbums(_self) {
+        spotify.configuration.spotifyApi.getAlbum(_self.songDetails.album.id)
+            .then(function (result) {
+                _self.album = result;
+        })
+    }
 
 
     return {

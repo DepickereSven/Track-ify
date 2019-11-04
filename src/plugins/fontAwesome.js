3de91cb693abe15bdf58c58fc0fe436615ceb1bd
svend <sven.depickere@student.howest.be>
2019-11-04 12:01:08 +0100

    Update fontAwesome.js

--- src/plugins/fontAwesome.js
+++ src/plugins/fontAwesome.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPlay } from '@fortawesome/free-solid-svg-icons'
 import { faStop } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { faSpotify } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faSpotify);
 library.add(faPlay);
 library.add(faStop);
+library.add(faArrowLeft);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);

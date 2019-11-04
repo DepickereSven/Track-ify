c82dec2bef6f478767e2b8a3d82aa7436061ee57
svend <sven.depickere@student.howest.be>
2019-11-04 12:07:10 +0100

    Create Genres.vue

---
+++ src/components/Genres.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="spanText">
+        <div v-if="genres.length === 1">
+            <span>{{genres[0]}}</span>
+        </div>
+        <div v-else v-for="(genre, i) in genres">
+            <span class="whitespace" v-if="genres.length > i + 1">{{genre}},</span>
+            <span v-else>{{genre}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "Genres",
+        props: ["genres"]
+    }
+
+</script>
+
+<style scoped>
+
+
+
+</style>

08a61b2383f788183ccde2713287ebfd459ca6f2
svend <sven.depickere@student.howest.be>
2019-11-04 11:09:18 +0100

    Update handle.js

--- src/assets/js/Vue/home/handle.js
+++ src/assets/js/Vue/home/handle.js
@@ -3,6 +3,7 @@
  **/
 
 import spotify from "../../spotify"
+import spotifyData from "./spotifyData"
 import songData from "../../../images/test"
 import artists from "../../../images/artists"
 
@@ -11,7 +12,7 @@ export default (function () {
     const init = function (_self) {
         _self.data = songData.items;
         _self.artists = artists.artists;
-        getAllGenres(_self);
+        spotifyData.getGenresAndPrepareCharData(_self);
         _self.loaded = true;
         // spotify.configuration.spotifyApi.getMyTopTracks({
         //     limit: 50,
@@ -22,13 +23,15 @@ export default (function () {
         //     return spotify.configuration.spotifyApi.getArtists(getIDsArtists(_self));
         // }).then(function(artistsInfo) {
         //     _self.artists = artistsInfo.artists;
-        //     getAllGenres(_self);
+        //     spotifyData.getGenresAndPrepareCharData(_self);
         //     _self.loaded = true;
         //     console.log("ready");
         // }).catch(function(error) {
         //     console.error(error);
         // });
     };
+
+    //**** HELP FUNCTIONS ****//
 
     const getIDsArtists = function (_self) {
         return makeString(_self.data.map(function (el) {
@@ -44,56 +47,14 @@ export default (function () {
         });
     }
 
-    function getAllGenres(_self) {
-        _self.genres = createNumberOfGenres(_self, _self.artists.map(function (el) {
-            return el.genres;
-        }));
-        getChartData(_self);
-    }
 
-    function createNumberOfGenres(_self, genres) {
-        let genreArray = [];
-        genres.forEach(function (el, topIndex) {
-            if (el.length !== 0){
-                el.forEach(function (element) {
-                    let index = genreArray.findIndex((genre => genre.name === element));
-                    if (index === undefined || index === -1){
-                        genreArray.push({
-                            name: element,
-                            hits: 1,
-                            // pictures: _self.artists[topIndex].images[0].url
-                        })
-                    } else {
-                        genreArray[index].hits++;
-                    }
-                })
-            }
-        });
-        return sort(genreArray);
-    }
 
-    function sort(list) {
-        return list.sort((a, b) => (a.hits < b.hits) ? 1 : -1)
-    }
 
-    function getChartData(_self) {
-        let index = 0;
-        while (index < 10){
-            _self.genresChart.labels.push(_self.genres[index].name);
-            _self.genresChart.datasets[0].backgroundColor.push(getRandomColor());
-            _self.genresChart.datasets[0].data.push(_self.genres[index].hits);
-            index++;
-        }
-    }
 
-    function getRandomColor() {
-        let letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
+
+
+
+
 
     return {
         init

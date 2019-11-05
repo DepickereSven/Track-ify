ff3e5d05a30d4089a9555ad21fb6f46ab61dbf8d
svend <sven.depickere@student.howest.be>
2019-11-05 11:34:01 +0100

    Update Details.vue

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,72 +1,31 @@
 <template>
     <v-container fluid fill-height>
         <v-layout align-center justify-center class="s-column">
-            <v-card class="card-v">
 
-                <div class="s-row">
-                    <div class="column first">
-                        <v-card-title class="display-2 font-weight-light">
-                            <div class="s-row">
-                                {{artists[0].name}}
-                                <v-chip class="ma-2" color="indigo" text-color="white">
-                                    <v-avatar left>
-                                        <font-awesome-icon :icon="['fas', 'heart']" size="3x"></font-awesome-icon>
-                                    </v-avatar>
-                                    {{formatNumbers}}
-                                </v-chip>
-                            </div>
-                        </v-card-title>
-                        <v-card-subtitle class="">
-                            <Genres :genres="artists[0].genres"></Genres>
-                        </v-card-subtitle>
+            <v-row align="center" class="detailsWidth">
+                <v-item-group v-model="window" class="shrink mr-6" mandatory tag="v-flex">
+                    <v-item v-for="n in length" :key="n" v-slot:default="{ active, toggle }">
+                        <div>
+                            <v-btn :input-value="active" icon @click="toggle">
+                                <font-awesome-icon :icon="['fas', 'circle']" size="lg"></font-awesome-icon>
+                            </v-btn>
+                        </div>
+                    </v-item>
+                </v-item-group>
 
-                        <v-list-item class="album-details">
+                <v-col>
 
-                            <v-list-item-avatar tile></v-list-item-avatar>
+                    <v-window v-model="window" reverse>
 
-                            <v-list-item-content>
-                                <div class="overline">{{album.album_type}} - {{album.release_date}}</div>
-                                <v-list-item-title class="headline mb-1">{{album.name}}</v-list-item-title>
-                                <v-list-item-subtitle>Number of Songs: {{album.total_tracks}}</v-list-item-subtitle>
-                            </v-list-item-content>
+                        <v-window-item v-for="(comp, n) in itemComponents" :key="n">
 
-                            <v-list-item-avatar tile size="80">
-                                <v-img :src="songDetails.album.images[0].url"></v-img>
-                            </v-list-item-avatar>
+                            <component :is="comp.name" v-bind="currentProperties(comp)" v-on="currentMethods(comp)"></component>
 
-                        </v-list-item>
+                        </v-window-item>
 
-                        <v-list rounded>
-                            <v-list-item-group color="primary">
-                                <v-list-item v-for="(song, i) in album.tracks.items" :key="i">
-
-                                    <v-list-item-avatar tile>
-                                        {{i + 1}}
-                                    </v-list-item-avatar>
-
-                                    <v-list-item-content>
-                                        <v-list-item-title v-text="song.name"></v-list-item-title>
-                                        <v-list-item-subtitle>
-                                            <ArtistsList class="artists" :artists="song.artists"></ArtistsList>
-                                        </v-list-item-subtitle>
-                                    </v-list-item-content>
-
-                                    <v-list-item-action @click="play(song)">
-                                        <font-awesome-icon :icon="['fas', 'play']" size="lg" pull="right"></font-awesome-icon>
-                                    </v-list-item-action>
-
-                                </v-list-item>
-                            </v-list-item-group>
-                        </v-list>
-                    </div>
-
-                    <div class="column second">
-                        <v-img :src="artists[0].images[0].url"></v-img>
-                    </div>
-                </div>
-            </v-card>
-
-            <Albums></Albums>
+                    </v-window>
+                </v-col>
+            </v-row>
 
             <audio :src="currentSong.current" @timeupdate="currentSong.time = $event.target.currentTime" ref="audio" @ended="musicEnded" autoload preload="auto" autoplay></audio>
             <MusicPlayer :current-song="currentSong" :time="currentSong.time" @stop="stopMusic"></MusicPlayer>
@@ -78,23 +37,32 @@
 <script>
 
     import handle from "../assets/js/Vue/details/handle"
-    import Genres from "../components/Genres"
-    import ArtistsList from "../components/ArtistsList"
+    import SongDetails from "../components/Details/SongDetails"
+    import Albums from "../components/Details/Albums"
     import MusicPlayer from "../components/MusicPlayer"
-    import Albums from "../components/Albums"
 
     export default {
         created: function () {
             handle.init(this);
         },
         components: {
-            Genres,
-            ArtistsList,
-            MusicPlayer,
-            Albums
+            SongDetails,
+            Albums,
+            MusicPlayer
         },
         // props: ["songDetails", "artists"],
         data: () => ({
+            itemComponents: [
+                {
+                    name: "SongDetails"
+                },
+                {
+                    name: "Albums"
+                }
+            ],
+            length: 2,
+            window: 0,
+
             songDetails: null,
             artists: null,
             album: {
@@ -131,15 +99,28 @@
             },
             musicEnded: function () {
                 this.currentSong.isPlaying = false;
-            }
-        },
-        computed: {
-            formatNumbers: function () {
-                // return this.$props.artists[0].followers.total.toLocaleString(
-                return this.artists[0].followers.total.toLocaleString(
-                    undefined,
-                    {minimumFractionDigits: 0}
-                );
+            },
+            currentProperties: function (comp) {
+                switch (comp.name) {
+                    case "SongDetails":
+                        return {
+                            // artists: this.$props.artists,
+                            artists: this.artists,
+                            album: this.album,
+                            // songDetails: this.$props.songDetails
+                            songDetails: this.songDetails
+                        };
+                    case "Albums":
+                        return {};
+                }
+            },
+            currentMethods: function (comp) {
+                switch (comp.name) {
+                    case "SongDetails":
+                        return {click: this.play};
+                    case "Albums":
+                        return {};
+                }
             }
         }
     }
@@ -147,32 +128,9 @@
 
 <style scoped>
 
-    .card-v {
+
+    .detailsWidth {
         width: 70%;
-    }
-
-    .card-v .s-row{
-        justify-content: space-between;
-    }
-
-    .card-v .s-row .column.first {
-        width: 65%;
-        padding-right: 4%;
-        padding-left: 2%;
-    }
-
-    .card-v .s-row .column.second {
-        width: 35%;
-        max-width: 350px;
-    }
-
-    .card-v .s-row .column.first .s-row {
-        justify-content: space-between;
-    }
-
-    .card-v .s-row .column.first .s-row.album-details {
-        padding-top: 5%;
-        padding-left: 16px;
     }
 
 </style>

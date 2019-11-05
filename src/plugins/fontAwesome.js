b68bd96a7b366cff564f05b104f4cd28d36e1676
svend <sven.depickere@student.howest.be>
2019-11-05 10:45:30 +0100

    new icon

--- src/plugins/fontAwesome.js
+++ src/plugins/fontAwesome.js
@@ -4,6 +4,7 @@ import { faPlay } from '@fortawesome/free-solid-svg-icons'
 import { faStop } from '@fortawesome/free-solid-svg-icons'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faCircle } from '@fortawesome/free-solid-svg-icons'
 import { faSpotify } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
@@ -12,5 +13,6 @@ library.add(faPlay);
 library.add(faStop);
 library.add(faArrowLeft);
 library.add(faHeart);
+library.add(faCircle);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);

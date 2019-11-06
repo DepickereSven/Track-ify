fca5d418c4b1034f062860e33042773ee5f5aced
svend <sven.depickere@student.howest.be>
2019-11-06 11:14:35 +0100

    init other artists

---
+++ src/components/Details/OtherArtists.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="s-row" v-if="otherArtists.length !== 0">
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "OtherArtists",
+        props: ["otherArtists"],
+        data: () => ({
+            
+        }),
+    }
+</script>
+
+<style scoped>
+
+</style>
